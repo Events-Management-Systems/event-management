a575309c205edb9c00cb08d21710f06a6b1046ac
nisargaram2003 <nnisargaram@gmail.com>
2025-10-03 12:39:18 +0530

    created eventPage

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,16 +2,29 @@ import axios from "axios"
 export class events{
     static SERVER_URL="http://localhost:5005"
 
-    // for displaying all event cards
-    static get allEvents(){
-        const dataUrl=`${this.SERVER_URL}/events`;
-        return axios.get(dataUrl)
-    }
+    // // for displaying all event cards
+    // static get allEvents(){
+    //     const dataUrl=`${this.SERVER_URL}/events`;
+    //     return axios.get(dataUrl)
+    // }
 
-    // for crweating new data to server (i.e, getting input data and adding new event)
-    // event is state that is used to create event in eventlist
-    static createEvent(event){
-        const dataURL=`${this.SERVER_URL}/events`
-        return axios.post(dataURL,event)    
-    }
+    // // for crweating new data to server (i.e, getting input data and adding new event)
+    // // event is state that is used to create event in eventlist
+    // static createEvent(event){
+    //     const dataURL=`${this.SERVER_URL}/events`
+    //     return axios.post(dataURL,event)    
+    // }
+    // Fetch all events
+  static get allEvents() {
+    return axios.get(`${this.SERVER_URL}/events`);
+  }
+
+  // Create new event
+  static createEvent(event) {
+    return axios.post(`${this.SERVER_URL}/events`, event);
+  }
+  // Optional: get event by ID
+  static getEventById(id) {
+    return axios.get(`${this.SERVER_URL}/events/${id}`);
+  }
 }
